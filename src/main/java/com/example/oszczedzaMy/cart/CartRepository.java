803b8a8d88aff9c567e3a01e5fcd88e90d17efde
Kordulasinski Jakub <kordulasinski.jakub@icloud.com>
2022-12-23 17:42:31 +0100

    Add repository to any created class so far

--- src/main/java/com/example/oszczedzaMy/cart/CartRepository.java
+++ src/main/java/com/example/oszczedzaMy/cart/CartRepository.java
@@ -1,8 +1,6 @@
 package com.example.oszczedzaMy.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CartRepository extends JpaRepository<Cart,Long> {
 }
